3a388013d40b6f2377a241698e72c23474b73fe7
swann-972 <swannperrot972@gmail.com>
2026-09-25 10:33:15 +0200

    Update program.d

--- program.c
+++ program.c
@@ -1,6 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+   int scoreJoueur = 0;
+   int scoreOrdi = 0;
+   int manche = 1;
+   int choixJoueur;
+   int choixOrdi;
+
+   printf("=== PIERRE - FEUILLE - CISEAUX (5 Manches) ===\n");
+   printf("Règles : 1 = Pierre, 2 = Feuille, 3 = Ciseaux\n\n");
+
+   while (manche <= 5) {
+      printf("--- Manche %d/5 ---\n", manche);
+
+      // Saisie du joueur
+      printf("Votre choix (1, 2 ou 3) : ");
+      scanf("%d", &choixJoueur);
+      // Choix aléatoire de l'ordinateur (1, 2 ou 3)
+      choixOrdi = (rand() % 3) + 1;
+      printf("L'ordinateur a choisi : %d\n", choixOrdi);
+      // Détermination du gagnant de la manche
+      if (choixJoueur == choixOrdi) {
+      printf("Égalité !\n");
+      } else if ((choixJoueur == 1 && choixOrdi == 3) ||
+      (choixJoueur == 2 && choixOrdi == 1) ||
+      (choixJoueur == 3 && choixOrdi == 2)) {
+      printf("Vous gagnez cette manche !\n");
+      scoreJoueur = scoreJoueur + 1;
+      } else {
+      printf("L'ordinateur gagne cette manche !\n");
+      scoreOrdi = scoreOrdi + 1;
+}
+      printf("Score actuel -> Vous : %d | Ordi : %d\n\n", scoreJoueur,scoreOrdi);
+
+manche = manche + 1;
+}
+// Bilan de la partie
+printf("=== FIN DE LA PARTIE ===\n");
+printf("Score final -> Vous : %d | Ordi : %d\n", scoreJoueur, scoreOrdi);
+if (scoreJoueur > scoreOrdi) {
+printf("Bravo, vous avez gagné la partie !\n");
+} else if (scoreOrdi > scoreJoueur) {
+printf("L'ordinateur remporte la partie...\n");
+} else {
+printf("Match nul parfait !\n");
+}
+return 0;
 }
